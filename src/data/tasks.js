6f007d5cfc7bc54ca284d465412611576c1afe7a
Luis <ltorrestomala@gmail.com>
2022-09-22 14:41:43 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "First Task",
+    description: "Description for my task",
+  },
+  {
+    id: 1,
+    title: "Second Task",
+    description: "Description for my task",
+  },
+  {
+    id: 2,
+    title: "Third Task",
+    description: "Description for my task",
+  },
+];
